97cebc061c5d3cd97076ec373ef2efa9d6945bcc
Barsha Shrestha <sbarsha@gmail.com>
2021-04-23 15:16:17 -0700

    removing caching from proxy

--- stressTest/getScript.js
+++ stressTest/getScript.js
@@ -5,9 +5,9 @@ export let options = {
   stages: [
     { duration: '1m', target: 1 }, // below normal load
     { duration: '1m', target: 10 },
-    { duration: '2m', target: 100 }, // normal load
-    { duration: '2m', target: 1000 },// around the breaking point
-    { duration: '2m', target: 100 },
+    { duration: '1m', target: 100 }, // normal load
+    { duration: '1m', target: 1000 },// around the breaking point
+    { duration: '1m', target: 100 },
     { duration: '1m', target: 10 },
     { duration: '1m', target: 1 },
     { duration: '1m', target: 0 }, // scale down. Recovery stage.
@@ -17,9 +17,13 @@ export let options = {
 
 export default function () {
 
-  var id = 9999991;
-  let res = http.get(`http://localhost:5000/rooms/${id}`);
-  check(res, { 'status was 200': (r) => r.status == 200 });
+  //var id = 9999991;
+  // let res = http.get(`http://localhost:5000/rooms/${id}`);
+  // check(res, { 'status was 200': (r) => r.status == 200 });
 
+  for (var id = 9000000; id < 9500000; id++) {
+    let res = http.get(`http://localhost:5000/rooms/${id}`);
+    check(res, { 'status was 200': (r) => r.status == 200 });
+  }
 
 }
